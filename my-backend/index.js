c0d310ea5e4cfda925f550e178616c61d5998038
Ritik Jain <ritik9628@gmail.com>
2025-02-11 22:47:36 +0530

    port changes

--- my-backend/index.js
+++ my-backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send({ msg: "Hello From Devops Backend." });
+  res.status(200).send({ msg: "Hello From Devops Backend." + process.env.MONGO_PASSWORD });
 });
 
 app.get("/data1", (req, res) => {
