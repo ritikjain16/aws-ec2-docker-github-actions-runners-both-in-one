b537432197c5edb1a51e6f1ad79587141eeb031b
Ritik Jain <ritik9628@gmail.com>
2025-02-11 08:23:31 +0530

    added backend in frontend

--- my-frontend/src/App.jsx
+++ my-frontend/src/App.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import myaxios from "./myaxios";
 
 const App = () => {
-  return <div>App {import.meta.env.VITE_API_URL}</div>;
+  const [mybackendData, setmybackendData] = useState({});
+  
+  const fetchBackend = async () => {
+    try {
+      const res = await myaxios.get("/");
+      console.log(res.data);
+      setmybackendData(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(()=>{
+    fetchBackend()
+  },[])
+
+  return (
+    <div>
+      App {import.meta.env.VITE_API_URL}
+      <h1>{mybackendData?.msg}</h1>
+    </div>
+  );
 };
 
 export default App;
