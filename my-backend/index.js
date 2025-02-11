76fc357e96fe57b8392b3eb48b234e476f507a7f
Ritik Jain <ritik9628@gmail.com>
2025-02-11 22:09:35 +0530

    port changes

--- my-backend/index.js
+++ my-backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-const port = process.env.PORT | 6000;
+const port = process.env.DOCKER_BACKEND_PORT | 5000;
 
 const app = express();
 
