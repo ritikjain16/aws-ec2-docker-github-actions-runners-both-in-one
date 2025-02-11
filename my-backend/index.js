d93166288ca3be15b9e4f85b7eacc2fa698311f9
Ritik Jain <ritik9628@gmail.com>
2025-02-11 23:04:34 +0530

    added mongodb

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import "./db/connecion.js";
 
 dotenv.config();
 
@@ -13,7 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send({ msg: "Hello From Devops Backend." + process.env.MONGO_PASSWORD });
+  res
+    .status(200)
+    .send({ msg: "Hello From Devops Backend." + process.env.MONGO_URL });
 });
 
 app.get("/data1", (req, res) => {
