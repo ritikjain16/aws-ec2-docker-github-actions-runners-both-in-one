250f965852a70ba237a84496db9979e48cf8f4ff
ritikjain16 <72817734+ritikjain16@users.noreply.github.com>
2025-02-12 08:14:47 +0530

    Update appMiddleware.js

--- my-backend/middleware/appMiddleware.js
+++ my-backend/middleware/appMiddleware.js
@@ -1,1 +1,19 @@
 
+import dotenv from "dotenv";
+dotenv.config();
+
+export const mymiddleware = (req, res, next) => {
+  if (
+    req.get("origin") === process.env.FRONTEND) {
+    next();
+  } else {
+    if (
+      req.headers.authorization ===
+      `${process.env.USERNAME_R}:${process.env.ADMINPASS}`
+    ) {
+      next();
+    } else {
+      res.status(400).send({ msg: "Authentication Error" });
+    }
+  }
+};
