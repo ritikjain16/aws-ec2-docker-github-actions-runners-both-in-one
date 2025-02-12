e0a30135d71cbbb560b50289e84f295316486b08
ritikjain16 <72817734+ritikjain16@users.noreply.github.com>
2025-02-12 08:18:17 +0530

    Update index.js

--- my-backend/index.js
+++ my-backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import "./db/connecion.js";
 import UserModel from "./models/user.js";
-
+import {appMiddleware} from "./middleware/appMiddleware.js";
 dotenv.config();
 
 const port = process.env.DOCKER_BACKEND_PORT | 5000;
@@ -37,6 +37,15 @@ app.get("/user/add", async (req, res) => {
       age: 25,
       email: "ritik9628@gmail.com",
     });
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get("/users/mid",appMiddleware, async (req, res) => {
+  try {
+    const users = await UserModel.find();
     res.status(200).send(users);
   } catch (e) {
     res.status(400).send(e);
